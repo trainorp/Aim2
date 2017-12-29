0cfb0c64beeda0beead840d790edf570d37b2cb1
trainorp <patrick.trainor@louisville.edu>
2017-12-29 00:30:03 -0500

    idk

--- makePackage.R
+++ makePackage.R
@@ -5,16 +5,24 @@ library(statmod)
 setwd("~/gdrive/Dissertation/Aim2/")
 
 devtools::create("BayesianGLasso")
-devtools::use_gpl3_license("BayesianGLasso")
+# devtools::create_description("BayesianGLasso")
+devtools::use_gpl3_license(pkg="BayesianGLasso")
 devtools::use_package("statmod",type="Imports",pkg="BayesianGLasso")
 devtools::use_package("MASS",type="Imports",pkg="BayesianGLasso")
 devtools::use_package("glasso",type="Imports",pkg="BayesianGLasso")
+# devtools::use_rcpp("BayesianGLasso")
+devtools::use_package("Rcpp",type="Imports",pkg="BayesianGLasso")
+devtools::use_package("RcppArmadillo",type="Imports",pkg="BayesianGLasso")
+devtools::use_package("RcppArmadillo",type="LinkingTo",pkg="BayesianGLasso")
+devtools::document("BayesianGLasso")
 devtools::use_cran_comments("BayesianGLasso")
-roxygen2::roxygenise(package.dir="~/gdrive/Dissertation/Aim2/BayesianGLasso")
+
+# roxygen2::roxygenise(package.dir="~/gdrive/Dissertation/Aim2/BayesianGLasso")
 
 currentCode<-as.package("BayesianGLasso")
+
 devtools::load_all(currentCode)
-devtools::document(currentCode)
 devtools::build(currentCode)
 devtools::build_win(currentCode)
+
 devtools::release("BayesianGLasso")
