8d314afcb90c23f809647e33f130f1d76dadc041
trainorp <patrick.trainor@louisville.edu>
2018-05-30 21:55:08 -0400

    idk

--- cVinesSims.R
+++ cVinesSims.R
@@ -1,8 +1,11 @@
+########### Prereqs ###########
 options(stringsAsFactors=FALSE,scipen=600)
 library(tidyverse)
 library(clusterGeneration)
 library(igraph)
+library(BayesianGLasso)
 
+########### Simulate data ###########
 nClust<-2
 nInClust<-20
 nSamp<-1000000
@@ -22,8 +25,14 @@ for(i in 1:length(sigmas)){
   end<-(i)*nInClust
   Sigmas[start:end,start:end]<-sigmas[[i]]
 }
+Conc<-solve(Sigmas)
 # heatmap(simData)
 
+########### BGL ###########
+bgl1<-blockGLasso(simData,iterations=1000,burnIn=0,adaptive=FALSE)
+bgl1Omegas<-bgl1$Omegas[[1000]]
+
+########### Evaluation ###########
 simDataCov<-cov(simData)
 simDataCor<-cor(simData)
 simDataConc<-solve(cov(simData))
@@ -35,3 +44,4 @@ plot(simDataCorG)
 simDataConcG<-graph_from_adjacency_matrix(abs(simDataConc),mode="undirected",
                                          diag=FALSE,weighted=TRUE)
 plot(simDataConcG)
+
